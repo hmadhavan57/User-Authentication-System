d441046943eaa36dd9c16afc2a8e5b47817c3a0a
maadhumaddy57@gmail.com <maadhumaddy57@gmail.com>
2024-05-15 12:49:10 +0530

    feat(style): adding style and content in login component

--- User_Authentication_ClientSide/src/Login.jsx
+++ User_Authentication_ClientSide/src/Login.jsx
@@ -1,9 +1,65 @@
-import React from "react";
-function Login(){
-return(
-    <div>
-       <p>Login</p>
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  
+  const [name, setName] = useState();
+  const [mobile, setMobile] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:3001/login',{email,password})
+    .then(result => {console.log(result)
+      navigate('/home')
+    })
+    .catch(err => console.log(err))
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-success vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>User Login</h2>
+        {error && <div className="alert alert-danger">{error}</div>}
+        <form onSubmit={handleSubmit}>
+          
+          
+          <div className="mb-3">
+            <label htmlFor="email"><strong>Email</strong></label>
+            <input
+              type="email"
+              placeholder="Enter Email Address"
+              autoComplete="off"
+              name="email"
+              className="form-control rounded-0"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password"><strong>Password</strong></label>
+            <input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              name="password"
+              className="form-control rounded-0"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-success w-100 rounded-0">Login</button>
+        </form>
+        <p className="mt-1">If you have an Account yet?</p>
+        <Link to="/register" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">Register</Link>
+      </div>
     </div>
-)
+  );
 }
-export default Login; 
+
+export default Login;
