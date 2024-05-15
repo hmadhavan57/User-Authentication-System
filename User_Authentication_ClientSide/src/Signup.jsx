e551cf209ca1fb7405e5f1cb300d5a5abd79ec45
maadhumaddy57@gmail.com <maadhumaddy57@gmail.com>
2024-05-15 12:31:34 +0530

    fix: fixing the navigation of the signup and login page

--- User_Authentication_ClientSide/src/Signup.jsx
+++ User_Authentication_ClientSide/src/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
   
@@ -9,11 +10,14 @@ function Signup() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [error, setError] = useState();
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     axios.post('http://localhost:3001/register',{name,mobile,email,password})
-    .then(result => console.log(result))
+    .then(result => {console.log(result)
+      navigate('/login')
+    })
     .catch(err => console.log(err))
   }
 
